4050af0b50095e19971af80ba45e5736ee862ec6
Tsukasa Noguchi <noguchi-tsukasa@dmm.com>
2024-08-07 16:13:36 +0900

    Update mainnet config

--- smp-oft/tasks/data/mainnet_config.ts
+++ smp-oft/tasks/data/mainnet_config.ts
@@ -5,13 +5,13 @@ const config = {
         'ethereum': {
             eid: 30101,
             endpointAddress: '0x1a44076050125825900e736c501f859c50fE728c',
-            oftAdapter: '<OFT Adapter Address>',
-            tokenAddress: '<SMP Token Address>', // Set the token address for the OFT adapter
+            oftAdapter: '0xcEb763AE6854796A00F8BAB60546960D0b272aAd',
+            tokenAddress: '0x7D36F7D8e9220F021305B8F13414C87DF688aa8B', // Set the token address for the OFT adapter
         },
         'optimism': {
             eid: 30111,
             endpointAddress: '0x1a44076050125825900e736c501f859c50fE728c',
-            oft: '<OFT Token Address>'
+            oft: '0x5f5292FE4583f476ab90Fbf247999816a9503F9E',
         },
     }
 }
