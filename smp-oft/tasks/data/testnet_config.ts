9f26e7a586513d5901bce33f9ae9c334b4142032
Tsukasa Noguchi <noguchi-tsukasa@dmm.com>
2024-10-29 16:07:33 +0900

    Update testnet addresses

--- smp-oft/tasks/data/testnet_config.ts
+++ smp-oft/tasks/data/testnet_config.ts
@@ -5,24 +5,19 @@ const config = {
         'sepolia': {
             eid: 40161,
             endpointAddress: '0x6EDCE65403992e310A62460808c4b910D972f10f',
-            oftAdapter: '<OFT Adapter Address>',
-            tokenAddress: '<SMP Token Address>', // Set the token address for the OFT adapter
+            oftAdapter: '0x5fbab85357E66Aef86e41652e47C2Ddf512040A1',
+            tokenAddress: '0xB2B0A672a8e5428f034B2eB1ed1978b25A108B27', // Set the token address for the OFT adapter
         },
         'op-sepolia': {
             eid: 40232,
             endpointAddress: '0x6EDCE65403992e310A62460808c4b910D972f10f',
-            oft: '<OFT Token Address>'
+            oft: '0xDC5d5E0D1FfE846310d0f3E6545456f80646AB0a'
         },
-        'amoy': {
-            eid: 40267,
-            endpointAddress: '0x6EDCE65403992e310A62460808c4b910D972f10f',
-            oft: '<OFT Token Address>'
-        },
-        // 'dmm-verse-testnet': {
-        //     eid: <endpoint_id>,
-        //     endpointAddress:  <address>,
-        //     oft: <address>
-        // }
+        'dm2-verse-testnet': {
+            eid: 40321,
+            endpointAddress:  '0x3aCAAf60502791D199a5a5F0B173D78229eBFe32',
+            oft: '0xEab312AD25Bf04216DEBA77F6cEa92631Cb75d63'
+        }
     }
 }
 
